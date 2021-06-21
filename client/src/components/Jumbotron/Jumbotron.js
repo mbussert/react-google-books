26616b0916db85a3a038c58261473b329e9cde49
Mike Bussert <mbussert@gmail.com>
2021-06-21 09:25:12 -0400

    Updated styling

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
 
 function Jumbotron() {
-    return (
-        <div>
-            <div className="jumbotron">
-                <h1 className="display-4 text-center myText">Google Books Search</h1>
-                <br></br>
-                <h2 className="text-center myText">Search for and save books of interest. Built with React.</h2>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-4 text-center myText">Google Books Search</h1>
+        <br></br>
+        <h2 className="text-center myText">
+          Search and save books of interest. Built with React.
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default Jumbotron;
